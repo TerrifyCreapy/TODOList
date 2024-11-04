d048f45235a3b6a7955b3cf92e11b18d107a2de6
terrifycreapy <e.konevtsev@gmail.com>
2024-11-04 09:49:26 +0300

    Routes changed and deploy.yml

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,7 +1,8 @@
-export const main_path = "/";
-export const auth_path = "/auth";
-export const todos_path = "/todos";
-export const yesterday_path = "yesterday";
-export const upcoming_path = "upcoming";
-export const today_path = "today";
-export const default_private_route = "todos"
+const baseUrl = import.meta.env.BASE || ""
+export const main_path = baseUrl + "/";
+export const auth_path = baseUrl + "/auth";
+export const todos_path = baseUrl + "/todos";
+export const yesterday_path = baseUrl + "yesterday";
+export const upcoming_path = baseUrl + "upcoming";
+export const today_path = baseUrl + "today";
+export const default_private_route = baseUrl + "todos"
